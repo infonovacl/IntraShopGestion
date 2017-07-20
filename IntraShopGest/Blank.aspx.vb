f8dc487f4f3163d4490d9c34ed557374132e3f9e
https://github.com/infonovacl/IntraShop.git <informatica@infonova.cl>
2017-07-19 20:27:28 -0400

    limando menu y ui de menu

--- IntraShopGest/Blank.aspx.vb
+++ IntraShopGest/Blank.aspx.vb
@@ -2,7 +2,9 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        Dim panelmenu As Menu
+        panelmenu = Master.FindControl("Menu1")
+        panelmenu.Visible = True
     End Sub
 
 End Class
--- IntraShopGest/Maestro.Master.vb
+++ IntraShopGest/Maestro.Master.vb
@@ -3,21 +3,23 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If IsPostBack = False And Page.User.Identity.IsAuthenticated = False Then
-            'Response.Write("<script>window.alert('MENSAJE1');</script>")
-            'Page.User.Identity.IsAuthenticated = False
-            ' Me.Panel_Login.Visible = False
-            ' Me.Panel_menu.Visible = False
-        ElseIf IsPostBack = False And Page.User.Identity.IsAuthenticated = True Then ' POSTBACK DESDE LOGIN
-            'Response.Redirect("Cliente.aspx", "_self")
-            'Response.Write("<script>window.alert('MENSAJE2');</script>")
-            Me.LBL_Usuario.Text = Session("nombreusuario")
-            Me.LBL_CTienda.Text = Session("codtienda")
-            Me.LBL_Caja.Text = Session("caja")
-            Me.LBL_NTienda.Text = Session("nombretienda")
-            Me.LBL_RUsu.Text = Session("usuario")
+
+        ElseIf IsPostBack = False And Page.User.Identity.IsAuthenticated = True Then ' POSTBACK DESDE LOGIN    
+            Me.Menu1.Visible = True
+            If Me.LBL_Usuario.Text = "" Then
+                If Session("usuario") Is System.DBNull.Value Then
+                Else
+                    Me.LBL_Usuario.Text = Session("nombreusuario")
+                    Me.LBL_CTienda.Text = Session("codtienda")
+                    Me.LBL_Caja.Text = Session("caja")
+                    Me.LBL_NTienda.Text = Session("nombretienda")
+                    Me.LBL_RUsu.Text = Session("usuario")
+                End If
+            Else
+            End If
         ElseIf IsPostBack = True Then
-            ' Response.Write("<script>window.alert('POST BACK TRU');</script>")
-        End If
+                ' Response.Write("<script>window.alert('POST BACK TRU');</script>")
+            End If
     End Sub
 
 End Class
